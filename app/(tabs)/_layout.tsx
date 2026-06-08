60bb78799cb908a4d51b9f3dbeed87f850542225
Nioh <47283088+ivanrenedo@users.noreply.github.com>
2026-06-08 14:21:52 +0100

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -35,7 +35,7 @@ export default function TabLayout() {
             <Search
               size={size}
               fill={focused ? color : 'transparent'}
-              strokeWidth={focused ? 0 : 1.5}
+              strokeWidth={focused ? .5 : 1.5}
             />
           ),
         }}
