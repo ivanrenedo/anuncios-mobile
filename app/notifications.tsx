fe12bc3e1cfedac8c569ca2cf88561309490bcfa
Nioh <47283088+ivanrenedo@users.noreply.github.com>
2026-06-08 15:29:15 +0100

    Updated notifications.tsx

--- app/notifications.tsx
+++ app/notifications.tsx
@@ -233,6 +233,7 @@ const styles = StyleSheet.create({
   headerLeft: {
     position: 'relative',
     display: 'flex',
+    justifyContent: 'center',
      alignItems: 'center',
     flexDirection: 'row'
   },
