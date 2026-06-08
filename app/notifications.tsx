8ac231d155e02f7d12959c2ca3d4d2850e43fcfc
Nioh <47283088+ivanrenedo@users.noreply.github.com>
2026-06-08 14:46:55 +0100

    Updated notifications.tsx

--- app/notifications.tsx
+++ app/notifications.tsx
@@ -215,8 +215,7 @@ const styles = StyleSheet.create({
     right: 0,
     zIndex: 50,
     backgroundColor: colors.surface + 'e6',
-    flexDirection: 'row',
-    alignItems: 'flex-end',
+   
     /* justifyContent: 'space-between', */
     paddingHorizontal: 12,
     paddingBottom: 10,
