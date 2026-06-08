4a88140af80a552bec4ae2143a8a9b95d9ad1a1e
Nioh <47283088+ivanrenedo@users.noreply.github.com>
2026-06-08 14:22:58 +0100

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -36,7 +36,7 @@ export default function TabLayout() {
               size={size}
               color={color}
               fill={focused ? color : 'transparent'}
-              strokeWidth={focused ? 1 : 1.5}
+              strokeWidth={focused ? 0 : 1.5}
             />
           ),
         }}
