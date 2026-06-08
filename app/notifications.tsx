ba7497ab53322db4879ce0ccba15c4cf475d1784
Nioh <47283088+ivanrenedo@users.noreply.github.com>
2026-06-08 14:43:22 +0100

    Updated notifications.tsx

--- app/notifications.tsx
+++ app/notifications.tsx
@@ -230,7 +230,8 @@ const styles = StyleSheet.create({
   },
   backBtn: {
     padding: 4,
-    
+    alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 18,
     width: 72,
   },
